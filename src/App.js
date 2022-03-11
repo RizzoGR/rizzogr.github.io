23cf24c3f00fa1a1910f86527f48e4f308a5b6c0
Gabriele Rizzo <GabrieleRizzo2b@gmail.com>
2022-03-11 21:05:14 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavigationBar from "./Layout/NavigationBar/NavigationBar";
+import HomePage from "./pages/HomePage/HomePage";
+
+function App() {
+    return (
+        <div>
+            <NavigationBar />
+            <HomePage />
+        </div>
+    );
+}
+
+export default App;
